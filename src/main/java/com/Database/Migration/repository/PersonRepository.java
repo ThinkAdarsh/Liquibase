67817cd6645aced2da22e266045114eb2b31062c
ThinkAdarsh <adarsh.mohanty@thinkitive.com>
2023-06-07 23:25:33 +0530

    feat implemented liquibase in spring boot

---
+++ src/main/java/com/Database/Migration/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.Database.Migration.repository;
+
+import com.Database.Migration.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+}
